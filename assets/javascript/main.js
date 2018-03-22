81ac6c9b8300dc94f4b212b5cc8912e8ca356dc2
Miriam Armada <miriam_armada@yahoo.com>
2018-03-21 18:35:56 -0700

    update 032118

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,7 +8,7 @@ var config = {
   };
   firebase.initializeApp(config);
 
-  $("#submit").on("click", function(){
+  $("#saveInfo").on("click", function(){
 
     var date = $("#date").val().trim();
     var inspector = $("homeInsp").val().trim();
@@ -17,10 +17,30 @@ var config = {
     var email = $("email").val().trim();
     var address = $("address").val().trim();
     var unitN = $("unitN").val().trim();
-    var city = $("city").val().trim()
-    var state = $("state").val().trim()
+    var city = $("city").val().trim();
+    var state = $("state").val().trim();
+    var zipCode =$("state").val().trim();
 
+    getRealStateInfo();
 
+    // write info in to Section 3, 
+    // pull value in to Temp Variables
+   
 
+    var type = $("#type");
+    var year = $("#year");
+    var sqFt = $("sqFt");
+    var bdr = $("#bdr");
+    var bath = $("#nBath");
+    var garage = $("#parking");
+    var lot = $("#lot");
+    var ac = $("#cooling");
+    var heat = $("#heat");
+    var roof = $("#roofing");
+    var flooring = $("#flooring");
+    var pool = $("#sPool");
 
+     // push info in Firebase Object
+
+     // grab uID
   });

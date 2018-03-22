4a7b2890c8788bcf325a707c5d72ada235664a83
Miriam Armada <miriam_armada@yahoo.com>
2018-03-21 21:20:23 -0700

    update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,46 +1,97 @@
 var config = {
-    apiKey: "AIzaSyAwGC7mDNrcI3JXveoAJhic3vVrQHuCt1Q",
-    authDomain: "inspectr-checkr.firebaseapp.com",
-    databaseURL: "https://inspectr-checkr.firebaseio.com",
-    projectId: "inspectr-checkr",
-    storageBucket: "inspectr-checkr.appspot.com",
-    messagingSenderId: "916021512160"
+  apiKey: "AIzaSyAwGC7mDNrcI3JXveoAJhic3vVrQHuCt1Q",
+  authDomain: "inspectr-checkr.firebaseapp.com",
+  databaseURL: "https://inspectr-checkr.firebaseio.com",
+  projectId: "inspectr-checkr",
+  storageBucket: "inspectr-checkr.appspot.com",
+  messagingSenderId: "916021512160"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+
+
+var homeInfo = {
+  type: "",
+  year: " ",
+  sqFt: " ",
+  bdr: " ",
+  bath: " ",
+  garage: " ",
+  lot: " ",
+  ac: " ",
+  heat: " ",
+  roof: " ",
+  flooring: " ",
+  pool: " ",
+};
+
+$("#saveInfo").on("click", function () {
+
+  event.preventDefault();
+
+  var date = $("#date").val().trim();
+  var inspector = $("#homeInsp").val().trim();
+  var jobNum = $("#jobNum").val().trim();
+  var name = $("#firstName").val().trim() + " " + $("#lastName").val().trim();
+  var email = $("#email").val().trim();
+  var address = $("#address").val().trim();
+  var unitN = $("#unitN").val().trim();
+  var city = $("#city").val().trim();
+  var state = $("#state").val().trim();
+  var zipCode = $("#zipCode").val().trim();
+
+  // getRealStateInfo(); // write info in to Section 3, // pull value in to home array
+
+  // create data base object
+  var newJob = {
+    date: date,
+    inspector: inspector,
+    jobNum: jobNum,
+    name: name,
+    email: email,
+    address: address,
+    unitN: unitN,
+    city: city,
+    state: state,
+    zipCode: zipCode,
+    homeInfo: homeInfo
+
   };
-  firebase.initializeApp(config);
 
-  $("#saveInfo").on("click", function(){
 
-    var date = $("#date").val().trim();
-    var inspector = $("homeInsp").val().trim();
-    var jobNum = $("jobNum").val().trim();
-    var name = $("firstName").val().trim() + " " + $("lastName").val().trim();
-    var email = $("email").val().trim();
-    var address = $("address").val().trim();
-    var unitN = $("unitN").val().trim();
-    var city = $("city").val().trim();
-    var state = $("state").val().trim();
-    var zipCode =$("state").val().trim();
+  // push info in Firebase Object
 
-    getRealStateInfo();
+  database.ref().push(newJob);
 
-    // write info in to Section 3, 
-    // pull value in to Temp Variables
-   
+  //var myUserId = firebase.auth().currentUser.uid;
+  console.log(database);
+  //console.log(myUserId);
 
-    var type = $("#type");
-    var year = $("#year");
-    var sqFt = $("sqFt");
-    var bdr = $("#bdr");
-    var bath = $("#nBath");
-    var garage = $("#parking");
-    var lot = $("#lot");
-    var ac = $("#cooling");
-    var heat = $("#heat");
-    var roof = $("#roofing");
-    var flooring = $("#flooring");
-    var pool = $("#sPool");
 
-     // push info in Firebase Object
+});
 
-     // grab uID
-  });
+
+//http://www.pngall.com/home-png/download/4409
+
+
+// function getRealStateInfo() {
+
+  /*ajax, bring info, response, write in to HTML
+
+
+  $("#type").text(homeInfo.type);
+  $("#year").text(homeInfo.year);
+  $("sqFt").text(homeInfo.sqFt);
+  $("#bdr").text(homeInfo.bdr);
+  $("#nBath").text(homeInfo.bath);
+  $("#parking").text(homeInfo.garage);
+  $("#lot").text(homeInfo.lot);
+  $("#cooling").text(homeInfo.ac);
+  $("#heat").text(homeInfo.heat);
+  $("#roofing").text(homeInfo.roof);
+  $("#flooring").text(homeInfo.flooring);
+  $("#sPool").text(homeInfo.pool);
+}*/
